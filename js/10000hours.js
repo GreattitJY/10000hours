5f377bba4e0ab1d2d47d57c32514a8335d2354d2
GreattitJY <great.tit.jaejae@gmail.com>
2022-10-24 22:44:50 +0900

    모달창 구현 완료, 로딩은 미구현

--- js/10000hours.js
+++ js/10000hours.js
@@ -1,1 +1,29 @@
-// js file
+// 모달창
+const mdLicat = document.querySelector(".modal-licat");
+const btnModal = document.querySelector(".btn-modal");
+const btnGo = document.querySelector(".btn-go");
+
+btnModal.addEventListener("click", () => {
+    mdLicat.style.display = "flex";
+});
+
+btnGo.addEventListener("click", () => {
+    mdLicat.style.display = "none";
+});
+
+window.onclick = function (event) {
+    if (event.target == mdLicat) {
+        mdLicat.style.display = "none";
+    }
+};
+
+// 로딩 이미지
+const loading = document.querySelector(".loading");
+const btnResult = document.querySelector(".btn-result");
+const inputs = document.querySelectorAll("input");
+
+function calc() {
+    console.log("호출 확인");
+    loading.style.display = "block";
+}
+btnResult.addEventListener("click", calc());
